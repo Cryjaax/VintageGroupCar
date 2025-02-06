da7535dfe9a057f1f8514a7d3a269b5e80e8657c
c.massa13 <c.massa13@studenti.unisa.it>
2025-02-06 20:28:50 +0100

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VintageGroupCar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
